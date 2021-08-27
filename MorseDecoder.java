fcdd506c2b68c36372ca201a2aef21d191fc30d6
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2021-08-27 15:07:32 -0600

    Morse Decoder Java

--- MorseDecoder.java
+++ MorseDecoder.java
@@ -1,44 +1,45 @@
+
+//Add Phidgets Library
 import com.phidget22.*;
 
 public class MorseDecoder {
-	// Global variables to turn on/off LEDS
+	//Global variables to turn on/off LEDS
 	static boolean turnRedLEDOn = false;
 	static boolean turnGreenLEDOn = false;
 
-	// Global variables to hold times
+	//Global variables to hold times
 	static long startPress;
 	static double timePressed;
 
-	// Global variable for the userInput
+	//Global variable for the userInput
 	static String userInput = "";
 
-	// Decoder method that will take the user input (code) and loop it through the
-	// morseCode array, looking for a match.
+	//Decoder method that will take the user input (code) and loop it through the
+	//morseCode array, looking for a match.
 	public static char Decoder(String code) {
 		String[] morseCode = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
 				"--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
 				"Unknown Code" };
 		int i = 0;
 		while (morseCode[i].equals("Unknown Code") == false) {
-			if (code.equals(morseCode[i])) // Once a match is found, the method will return the character corresponding
-											// to that code.
-			{
-				return Character.toUpperCase((char) (97 + i)); // Character 'a' starts at 97.
+			if (code.equals(morseCode[i])){ //Once a match is found, the method will return the character corresponding to that code. 								
+				return Character.toUpperCase((char) (97 + i)); //Character 'a' starts at 97.
 			}
 			i++;
 		}
-		return '?';// unknown code
+		return '?'; //unknown code
 	}
 
-	// Handle Exceptions
+	//Handle Exceptions
 	public static void main(String[] args) throws Exception {
-		// Create
+
+		//Create
 		DigitalInput redButton = new DigitalInput();
 		DigitalOutput redLED = new DigitalOutput();
 		DigitalInput greenButton = new DigitalInput();
 		DigitalOutput greenLED = new DigitalOutput();
 
-		// Address
+		//Address
 		redLED.setHubPort(1);
 		redLED.setIsHubPortDevice(true);
 		redButton.setHubPort(0);
@@ -48,60 +49,55 @@ public class MorseDecoder {
 		greenLED.setHubPort(4);
 		greenLED.setIsHubPortDevice(true);
 
-		// Event
+		//Event
 		redButton.addStateChangeListener(new DigitalInputStateChangeListener() {
 			public void onStateChange(DigitalInputStateChangeEvent e) {
-				turnRedLEDOn = e.getState(); // Turns on red LED
-				if (e.getState()) // Once red button is pushed, Decoder method is called for the users input.
-				{
+				turnRedLEDOn = e.getState(); //Turns on red LED
+				if (e.getState()) { //Once red button is pushed, Decoder method is called for the users input.
 					System.out.print(" = ");
 					System.out.print(Decoder(userInput));
 					System.out.println();
-					userInput = ""; // Clears userInput for next code
+					userInput = ""; //Clears userInput for next code
 				}
 			}
 		});
 
-		// Event
+		//Event
 		greenButton.addStateChangeListener(new DigitalInputStateChangeListener() {
 			public void onStateChange(DigitalInputStateChangeEvent e) {
-				turnGreenLEDOn = e.getState(); // Turns on green LED
+				turnGreenLEDOn = e.getState(); //Turns on green LED
 				if (e.getState()) {
-					startPress = System.currentTimeMillis(); // Grabs time when the button is first pressed
+					startPress = System.currentTimeMillis(); //Grabs time when the button is first pressed
 				}
 				if (e.getState() == false) {
-					timePressed = ((double) (System.currentTimeMillis() - startPress) / 1000); // Grabs time when the
-																								// button is released,
-																								// and subtracts start
-																								// time. Converts from
-																								// ms to seconds.
+					timePressed = ((double) (System.currentTimeMillis() - startPress));//Grabs time when button released, subtracts start time.
+				}
+				if ((timePressed < 600) && (timePressed > 100)) {
+					userInput += ".";
+					System.out.print("."); //Prints a dot to the screen
+				} else if (timePressed > 600 && timePressed < 5000) {
+					userInput += "-";
+					System.out.print("-"); //Prints a dash to the screen
 				}
 			}
 		});
 
-		// Open
+		//Open
 		redLED.open(1000);
 		greenLED.open(1000);
 		redButton.open(1000);
 		greenButton.open(1000);
 
+		//Use your Phidgets
 		System.out.println(
 				"Start entering into the decoder." + "\n" + "Press the green button for a dot, hold for a dash." + "\n"
 						+ "Press the red button after you enter a letter.");
-		while (true) // According to how long the user holds the button down for, it will add a dot
-						// or a dash to the userInput string, as well as print it to the screen. Also
-						// controls LEDS
-		{
+		while (true){ //According to how long the user holds the button down for, it will add a dot
+						//or a dash to the userInput string, as well as print it to the screen. Also
+						//controls LEDS
 			timePressed = 0;
 			redLED.setState(turnRedLEDOn);
 			greenLED.setState(turnGreenLEDOn);
-			if (timePressed < 0.6 && timePressed > 0.1) {
-				userInput += ".";
-				System.out.print("."); // Prints a dot to the screen
-			} else if (timePressed > 0.6) {
-				userInput += "-";
-				System.out.print("-"); // Prints a dash to the screen
-			}
 		}
 	}
 }
